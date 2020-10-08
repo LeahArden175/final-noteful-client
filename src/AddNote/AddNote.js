e9f1423d4ab79f82f136249344a956de771317fb
Leah Arden <leaharden175@gmail.com>
2020-10-08 14:19:03 -0400

    fetch work

--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -1,16 +1,39 @@
 import React from "react";
 import "./AddNote.css";
 import ApiContext from '../ApiContext';
+import config from '../config';
 
 export default class AddNote extends React.Component {
   handleSubmit = (event) => {
     event.preventDefault();
     console.log(this.state);
-
+  }
+/*
+  fetch(`${config.API_ENDPOINT}/notes/${noteId}` {
+    method: 'DELETE',
+    headers: {
+      'content-type': 'application/json'
+    },
+  })
+    .then(res => {
+      if (!res.ok)
+        return res.json().then(e => Promise.reject(e))
+      return res.json()
+    })
+    .then(() => {
+      this.context.deleteNote(noteId)
+      // allow parent to perform extra behaviour
+      this.props.onDeleteNote(noteId)
+    })
+    .catch(error => {
+      console.error({ error })
+    })
+}
+*/
     //using values from the state, call addNote on the context, add on change listeners and add a function to detect when those fields are changing, properties to add are three inputs in the return statement
-  };
+
   render() {
-    const { className, ...otherProps } = this.props;
+    const { className } = this.props;
     return (
       <ApiContext.Consumer>
         {({ folders }) => (
@@ -20,13 +43,13 @@ export default class AddNote extends React.Component {
             action="#"
           >
             <label>Name</label>
-            <input type="text" />
+            <input type="text" id="noteName"/>
             <label>Content</label>
-            <input type="text" />
+            <input type="text" id="noteContent"/>
             <label>Folder</label>
             <select>
               {folders.map((folder) => (
-                <option value={folder.id}>{folder.name}</option>
+                <option value={folder.id} id="noteFolder">{folder.name}</option>
               ))}
             </select>
             <button type="submit">Submit</button>
